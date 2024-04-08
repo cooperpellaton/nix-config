81695baaec1711872e2dcdec02d2aacb8e1142d5
Cooper Pellaton <c@cepp.ch>
2024-04-07 19:24:16 -0700

    Update work email.

--- work.nix
+++ work.nix
@@ -1,6 +1,9 @@
-{ pkgs, config, lib, ... }:
-
 {
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
   # have to do this for terraform
   nixpkgs.config.allowUnfree = true;
 
@@ -28,6 +31,6 @@
   ];
 
   programs = {
-    git.userEmail = lib.mkForce "cooper.pellaton@hu.ma.ne";
+    git.userEmail = lib.mkForce "cooper@Humane.com";
   };
 }
