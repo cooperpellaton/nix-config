03f04641c100b8ccfc8db4e12ac26ebd2f0b3e48
Cooper Pellaton <c@cepp.ch>
2024-04-16 00:21:15 -0700

    Remove emacs inputs.

--- flake.nix
+++ flake.nix
@@ -7,20 +7,11 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    doom-emacs = {
-      url = "github:nix-community/nix-doom-emacs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    emacs = {
-      url = "github:nix-community/emacs-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
     nixpkgs,
     home-manager,
-    doom-emacs,
     ...
   } @ inputs: {
     packages = {
@@ -36,12 +27,10 @@
       pkgs = nixpkgs.legacyPackages.aarch64-darwin;
       modules = [
         ./home.nix
-        doom-emacs.hmModule
         {
           home.username = "cooper";
           home.homeDirectory = "/Users/cooper";
         }
-        {nixpkgs.overlays = [inputs.emacs.overlay];}
       ];
     };
 
@@ -50,7 +39,6 @@
       modules = [
         ./home.nix
         ./work.nix
-        doom-emacs.hmModule
         {
           home.username = "cooper";
           home.homeDirectory = "/Users/cooper";
@@ -63,7 +51,6 @@
       modules = [
         ./home.nix
         ./work.nix
-        doom-emacs.hmModule
         {
           home.username = "cooper";
           home.homeDirectory = "/Users/cooper";
