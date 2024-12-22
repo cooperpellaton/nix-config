54ba399aaf4c022188812da82cae79f15cf8358e
Cooper Pellaton <c@cepp.ch>
2024-12-22 00:13:26 -0800

    Moved vim config to it's own file.
    Also cleaned up the fish interactive init.

--- home.nix
+++ home.nix
@@ -7,6 +7,7 @@
   isDarwin = pkgs.stdenv.isDarwin;
   doomPath = "${config.home.homeDirectory}/.config/home-manager/doom";
 in {
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "24.11";
   programs.home-manager.enable = true;
 
@@ -104,10 +105,12 @@ in {
       shellAbbrs = {ec = "emacsclient -nw -c -a 'emacs'";};
       loginShellInit = "zoxide init fish | source";
       interactiveShellInit =
-        lib.mkIf isDarwin
         ''
-          fish_add_path /opt/homebrew/bin
+          fish_add_path ~/.config/emacs/bin
           batman --export-env | source
+        ''
+        + lib.optionalString isDarwin ''
+          fish_add_path /opt/homebrew/bin
         '';
     };
     git = {
@@ -147,102 +150,14 @@ in {
         };
       };
     };
-    neovim = {
+    neovim = let
+      vimConfig = (import ./vim.nix) {inherit pkgs;};
+    in {
       enable = true;
       vimAlias = true;
       defaultEditor = true;
-      extraConfig = ''
-        """""""""""
-        " GENERAL "
-        """""""""""
-        set clipboard=unnamed
-        set number relativenumber
-        augroup numbertoggle
-          autocmd!
-          autocmd BufEnter,FocusGained,InsertLeave * set relativenumber
-          autocmd BufLeave,FocusLost,InsertEnter   * set norelativenumber
-        augroup END
-        syntax on
-        set t_Co=256
-        " Fix backspace so that it works normally
-        set backspace=indent,eol,start
-        " Show existing tab with 2 spaces width
-        set tabstop=2
-        " Copy indent from current line when starting a new line.
-        set autoindent
-        " when indenting with '>', use 2 spaces width
-        set shiftwidth=2
-        " On pressing tab, insert 2 spaces
-        set expandtab
-        " Turn case sensitive search off and smartcase search on
-        set ignorecase
-        set smartcase
-        " Fold based on syntax
-        set foldmethod=syntax
-        " Don't fold files by default
-        set nofoldenable
-        " Don't show the current mode on the last line (status line displays the current mode)
-        set noshowmode
-        " Limit the completion menu to 10 entries
-        set pumheight=10
-        " pres // to stop highlighting results
-        nmap <silent> // :nohlsearch<CR>
-
-        """""""""""""""
-        " Indent Line "
-        """""""""""""""
-        " Change the characters recursively
-        let g:indentLine_char_list = ['|', '¦', '┆', '┊']
-
-        """""""""""""""""
-        " WINDOW CONFIG "
-        """""""""""""""""
-        " quicker window movement
-        nnoremap <C-j> <C-w>j
-        nnoremap <C-k> <C-w>k
-        nnoremap <C-h> <C-w>h
-        nnoremap <C-l> <C-w>l
-
-        " open new split panes to right and bottom, which feels more natural
-        set splitbelow
-        set splitright
-      '';
-      plugins = with pkgs.vimPlugins; [
-        vim-fugitive
-        supertab
-        {
-          plugin = lightline-vim;
-          config = ''
-            let g:lightline = {
-                  \ 'colorscheme': 'selenized_dark',
-                  \ 'active': {
-                  \   'left': [ [ 'mode', 'paste' ],
-                  \             [ 'gitbranch', 'readonly', 'filename', 'modified' ] ]
-                  \ },
-                  \ 'component_function': {
-                  \   'gitbranch': 'FugitiveHead'
-                  \ },
-                  \ }
-          '';
-        }
-        vim-gitgutter
-        vim-wordmotion
-        vim-endwise
-        float-preview-nvim
-        {
-          plugin = echodoc;
-          config = ''
-            let g:echodoc#enable_at_startup = 1
-            let g:echodoc#type = 'floating'
-            " To use a custom highlight for the float window,
-            " change Pmenu to your highlight group
-            highlight link EchoDocFloat Pmenu
-          '';
-        }
-        tagbar
-        vim-smoothie
-        zellij-nvim
-      ];
+      extraConfig = vimConfig.config;
+      plugins = vimConfig.plugins;
     };
     zellij = {
       enable = true;
